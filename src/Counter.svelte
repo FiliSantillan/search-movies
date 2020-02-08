5495c58f348e05cee9487282c2f605b08f23ccee
Fili Santillán <fili06604@gmail.com>
2020-02-08 04:00:28 -0600

    :sparkles: Add counter with Svelte

---
+++ src/Counter.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let initialCounter = 0;
+  let count = initialCounter;
+
+  function handleClick() {
+    count++;
+  }
+</script>
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
+
+<button on:click={handleClick}>Incrementar</button>
+<h1>{count}</h1>
